40e19e9188bfa05cf4cd949768029b86d57dbd2f
dpansheriya <dpansheriya@localhost>
2014-03-03 12:13:35 +0530

    Making Croping to work with zoom

--- www/scripts/indexJS.js
+++ www/scripts/indexJS.js
@@ -6,7 +6,15 @@ var cropX;
 var cropY;
 var cropW;
 var cropH;
-var canvasHidden;
+var canvas;
+
+var boundX,BoundY;
+var xsize,ysize;
+var jcrop_api;
+var  finalSelection;
+
+var crop_img;
+
 
 function onLoad() {
 	console.log("Called Load...");
@@ -17,7 +25,7 @@ function onLoad() {
 
 function onPhotoDataSuccess(imageData) {
 	console.log("image: " + imageData);
-	$('#smallImage').attr('src',imageData);
+	$('#smallImage').attr('src',"data:image/jpeg;base64," + imageData);
 //	$('#cropImage').attr('src',imageData);
 	loadPage("imagePrev");
 }
@@ -26,7 +34,6 @@ function onDeviceReady() {
 	console.log("Ready...");
 	pictureSource = navigator.camera.PictureSourceType;
 	destinationType = navigator.camera.DestinationType;
-	canvasHidden = document.createElement('canvas');
 	loadPage("home");
 }
 
@@ -34,7 +41,7 @@ function captureI() {
 	console.log("CaptureI");
 	navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
 		quality : 90,
-		destinationType : Camera.DestinationType.FILE_URI,
+		destinationType : Camera.DestinationType.DATA_URL,
 		allowEdit : true
 	});
 }
@@ -75,20 +82,51 @@ function onCrop()
 {
 	
 	loadPage('cropView');
-	var canvas = document.createElement('canvas');
-	$('#cropImage').html(['<img src="', document.getElementById('smallImage').src, '" width="80%" height="80%"/>'].join(''));
-	var img = $('#cropImage img')[0];
+	canvas = document.createElement('canvas');
+	var tempImage = new Image();
+    tempImage.src = $('#cropImage').src;
+    
+//    var widthAct = tempImage.width;
+//    var heightAct = tempImage.height;
+//    
+//    console.log("widthAct = " +widthAct);
+//    console.log("heightAct = " +heightAct);
+//    
+//    var widthImg = $('#cropImage').width;
+//    var heightImg = $('#cropImage').height;
+//    
+//    console.log("widthImg = " +widthImg);
+//    console.log("heightImg = " +heightImg);
+   
+   
+	$('#cropImage').html(['<img src="', document.getElementById('smallImage').src, '" width="80%" height="80%" />'].join(''));
+	crop_img = $('#cropImage img')[0];
+	
+	var tempImage = new Image();
+    tempImage.src = crop_img.src;
+	xsize = tempImage.width,
+    ysize = tempImage.height;
+	
+	
 	$('#cropImage img').Jcrop({
 						 bgColor: 'black',
 						 bgOpacity: .3,
 						 onSelect: cropAreaChanged,
 						 onChange: cropAreaChanged
-					});
+					},function(){
+					      // Use the API to get the real image size
+					      var bounds = this.getBounds();
+					      boundx = bounds[0];
+					      boundy = bounds[1];
+					      // Store the API in the jcrop_api variable
+					      jcrop_api = this; 
+					    });
 	
 	function cropAreaChanged(selection)
 	{
-		if (selection.w < 5)
-			selection.w = 5;
+		if (selection.w > 0 && selection.h > 0 )
+			finalSelection = selection;
+			/*selection.w = 5;
 		if (selection.h < 5)
 			selection.h = 5; 
 		
@@ -97,5 +135,50 @@ function onCrop()
 		
 		var ctx = canvas.getContext('2d');
 		ctx.drawImage(img, selection.x, selection.y, selection.w, selection.h, 0, 0, canvas.width, canvas.height);
+		*/
 	}
+	
 }	
+
+function onCropSaved()
+{
+	canvas = document.createElement('canvas');
+	console.log(canvas.toDataURL());
+	
+	
+	
+	var rx = xsize/boundx ;
+    var ry = ysize/boundy ;
+
+    console.log("***rx1***: " + rx);
+	console.log("***ry1***" + ry);
+    
+	canvas.width =  Math.round(rx * finalSelection.w);
+	canvas.height = Math.round(ry * finalSelection.h);
+
+	var x1 =Math.round(rx * finalSelection.x);
+	var y1 = Math.round(ry * finalSelection.y);
+	
+	var w = canvas.width ;
+	var h =canvas.height;
+	
+	console.log("***W*** : " + w);
+	console.log("***H***: " + h);
+	console.log("***X1*** : " + x1);
+	console.log("***Y1***: " + y1);
+	console.log("***finalSelection.x*** : " + finalSelection.x);
+	console.log("***rx2***: " + rx);
+	console.log("***ry2***" + ry);
+	console.log("***xsize***: " + xsize);
+	console.log("***ysize***: " + ysize);
+	console.log("***boundX***: "+ boundx);
+	console.log("***boundY***: "+ boundy);
+	
+	
+	
+	var ctx = canvas.getContext('2d');
+	ctx.drawImage(crop_img, x1, y1, w,h, 0, 0, w, h);
+	
+	$('#smallImage').attr('src',canvas.toDataURL());
+	loadPage("imagePrev");
+}
