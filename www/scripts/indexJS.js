bbabca3396d8e42bfa5ecdb98034a0db1429b65e
Milap <Milap@localhost>
2014-03-07 12:04:10 +0530

    Add WaterMark Effect On Actual Image

--- www/scripts/indexJS.js
+++ www/scripts/indexJS.js
@@ -11,10 +11,28 @@ var finalSelection;
 var crop_img;
 
 
+var watermark;
+var watermarkX = "10";
+var watermarkY = "10";
+var opacity = (255/(100/50));
+var gcanvas;
+var gctx;
+var origImg;
+//var waterImg;
+var previewCanvas;
+var contexto;
+var canvaso;
+var wrx;
+var wry;
+var wmCanvasDiv;
+
+
 function onLoad() {
 	loadPage("home");
 	document.addEventListener("deviceready", onDeviceReady, false);
 	document.addEventListener("backbutton", onBackButton, false);
+	watermark = new Image();
+	watermark.src = "img/logo.png";
 }
 
 function onPhotoDataSuccess(imageData) {
@@ -121,7 +139,15 @@ function onCropSaved() {
 	var ctx = canvas.getContext('2d');
 	ctx.drawImage(crop_img, x1, y1, w, h, 0, 0, w, h);
 	$('#waterImage').attr('src', canvas.toDataURL());
+	origImg = new Image();
+	origImg.src=canvas.toDataURL();
+	
 	loadPage("waterImgPrev");
+	onWatermark();
+	//Using timer to reApplyWaterMark
+	window.setTimeout(function(){
+	reApplyatterMark();
+	},300);
 }
 
 function reonCrop(){
@@ -134,6 +160,87 @@ function onCropSkip(){
 	$('#waterImage').attr('src',document.getElementById('smallImage').src);
 	loadPage("waterImgPrev");
 }
+//For Water Mark
+function onWatermark(){
+	
+	initCanvas();
+	initWatermark();	
+}
+function initCanvas(){
+	
+			wmCanvasDiv = document.getElementById('wmCanvasContainer');
+			gcanvas =  document.getElementById('WatermarkCanvas'); //document.createElement('canvas');
+			if (!gcanvas) {
+			  alert('Error: I cannot create a new canvas element!');
+			  return;
+			}
 
+			//gcanvas.style.cssText = " width:80%; height:80%";
+			gctx = gcanvas.getContext("2d");
+			
+			
+			gcanvas.onmouseup = function(ev){
+				var x,y;
+				if (ev.layerX || ev.layerX == 0) { // Firefox
+				  x = ev.layerX;
+				  y = ev.layerY;
+				} else if (ev.offsetX || ev.offsetX == 0) { // Opera
+				  x = ev.offsetX;
+				  y = ev.offsetY;
+				}
+				applyWatermark(x,y);
+			}
+}
+function initWatermark(){
+	
+	applyTransparency();
+			console.log("watermark img="+watermark.src);
+}
+function applyTransparency(){
+			var w = watermark.width || watermark.offsetWidth,
+			h = watermark.height || watermark.offsetHeight;
+			
+			canvas.width = w;
+			canvas.height = h;
+			var ctx = canvas.getContext('2d');
+			ctx.drawImage(watermark, 0, 0);
+			var image = ctx.getImageData(0, 0, w, h);
+			var imageData = image.data,
+			length = imageData.length;
+			for(var i=3; i < length; i+=4){  
+				imageData[i] = (imageData[i]<opacity)?imageData[i]:opacity;
+			}
+			image.data = imageData;
+			ctx.putImageData(image, 0, 0);
+			watermark.onload = null;
+			watermark.src = "";
+			watermark.src = canvas.toDataURL();
+			// assign img attributes to the transparent watermark
+			// because browsers recalculation doesn't work as fast as needed
+			watermark.width = w;
+			watermark.height = h;
+			applyWatermark(50,50);
+}
+
+function applyWatermark(x,y){
+	console.log("applyWatermark");
+		
+			gcanvas.width = origImg.width || origImg.offsetWidth;
+			gcanvas.height = origImg.height || origImg.offsetHeight;
+			gctx.drawImage(origImg, 0, 0);
+			
+			x=x-(watermark.width/2);
+			y=y-(watermark.height/2);
+			if(x<0)
+				x=0;
+			if(y<0)
+				y=0;
+			gctx.drawImage(watermark, x, y);
+			
+}
+function reApplyatterMark(){
+
+	applyWatermark(50,50);
+}
 
 
