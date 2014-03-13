cc5fd4b66b0bf40f28a2c98f972f4dd4f6f89443
Milap <Milap@localhost>
2014-03-13 18:25:10 +0530

    login page successfully work.

--- www/scripts/indexJS.js
+++ www/scripts/indexJS.js
@@ -17,34 +17,82 @@ var gcanvas;
 var gctx;
 var origImg;
 //var waterImg;
-var scW;
-var scH;
 var fileName;
 var imageURI;
 var db;
 var vis_url,vis_lang,vis_client_id,vis_role,vis_whouse_id,vis_ord_id;
+var username;
 
 
 function onLoad() {
-	loadPage("home");
 	document.addEventListener("deviceready", onDeviceReady, false);
 	document.addEventListener("backbutton", onBackButton, false);
-	scW=window.innerWidth;
-	scH=window.innerHeight;
 	watermark = new Image();
 	origImg = new Image();
 	watermark.src = "img/Velocity_Watermark.png";
 }
-
-
 function onLoginpage(){
 	loadPage("login");
 }
 function onLogin(){
-	
+
+	$.ajax({type: "POST",
+			url: "http://192.168.0.121:8088/VISService/services/ModelADService",
+			dataType: "xml",
+			contentType: 'text/xml; charset=utf-8',
+			data: '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:_0="http://idempiere.org/ADInterface/1_0">'
+				   +'<soapenv:Header/>'
+				   +'<soapenv:Body>'
+					  +'<_0:readData>'
+						 +'<_0:ModelCRUDRequest>'
+							+'<_0:ModelCRUD>'
+							   +'<_0:serviceType>Login</_0:serviceType>'
+							   +'<_0:TableName>AD_User</_0:TableName>'
+							   +'<_0:recordIDVariable>@##AD_User_ID</_0:recordIDVariable>'
+							   +'<_0:DataRow>'
+							   +'</_0:DataRow>'
+							+'</_0:ModelCRUD>'
+							+'<_0:ADLoginRequest>'
+							   +'<_0:user>'+document.getElementById("txt_user").value+'</_0:user>'
+							   +'<_0:pass>'+document.getElementById("txt_password").value+'</_0:pass>'
+							   +'<_0:lang>'+vis_lang+'</_0:lang>'
+							   +'<_0:ClientID>'+vis_client_id+'</_0:ClientID>'
+							   +'<_0:RoleID>'+vis_role+'</_0:RoleID>'
+							   +'<_0:OrgID>'+vis_ord_id+'</_0:OrgID>'
+							   +'<_0:WarehouseID>'+vis_whouse_id+'</_0:WarehouseID>'
+							   +'<_0:stage>9</_0:stage>'
+							+'</_0:ADLoginRequest>'
+						 +'</_0:ModelCRUDRequest>'
+					  +'</_0:readData>'
+				   +'</soapenv:Body>'
+				+'</soapenv:Envelope>',
+                    success: processSuccess,
+                    error: processError
+                });
+				function processSuccess(data, status, req) {
+                    if (status == "success")
+					{
+						if($(req.responseXML).find('WindowTabData').find('RowCount').text()=="1")
+						{
+							username=$(req.responseXML).find('WindowTabData').find('DataSet').find('DataRow').find('field').find('val').text();
+							loadPage("home");
+							document.getElementById("user_lbl").innerHTML="Hello : "+username;
+						}
+						else{
+							//navigator.notification.alert(req.responseText + " " + status);
+							loadPage("login");
+							document.getElementById("login_error").innerHTML="Not Valid User";
+						}
+					}
+					
+                }
+
+                function processError(data, status, req) {
+					loadPage("login");
+					document.getElementById("login_error").innerHTML="Something Going Wrong !!!";
+                }  
 }
 function onSettingPage(){
-	console.log("url="+vis_url+"\nlang="+vis_lang+"\nclient="+vis_client_id+"\nrole="+vis_role+"\nwarehouse="+vis_whouse_id+"\norg="+vis_ord_id);
 	loadPage("setting");
 	setSettingpage();
 }
@@ -63,14 +111,13 @@ function onSettingUpdate(){
 	vis_role=document.getElementById("txt_role").value;
 	vis_whouse_id=document.getElementById("txt_warehouse").value;
 	vis_ord_id=document.getElementById("txt_organizer").value;
-	console.log("url="+vis_url+"\nlang="+vis_lang+"\nclient="+vis_client_id+"\nrole="+vis_role+"\nwarehouse="+vis_whouse_id+"\norg="+vis_ord_id);
 	if(vis_url=="" && vis_lang=="" && vis_client_id=="" && vis_role=="" && vis_whouse_id=="" && vis_ord_id=="")
 	{
 		navigator.notification.alert('No Any Field Should Blank!',onSettingPage,'Invalid Value','Ok');
 		successCB();
 	}else{
 		db.transaction(updateSettingDb, errorCB);
-		onLoginpage();
+		loadPage("login");
 	}
 	
 }
@@ -85,7 +132,7 @@ function onDeviceReady() {
    	db.transaction(populateDB, errorCB, successCB);
 	pictureSource = navigator.camera.PictureSourceType;
 	destinationType = navigator.camera.DestinationType;
-	loadPage("home");
+	loadPage("login");
 }
 //db process
 function errorCB(err) {
