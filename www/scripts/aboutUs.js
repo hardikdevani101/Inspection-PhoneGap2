34ada0290ed62c3f0931be95c99ea71553399e7a
Vijay <Vijay@localhost>
2014-07-09 19:34:13 +0530

    Modify code for initialize aboutUs page.

--- www/scripts/aboutUs.js
+++ www/scripts/aboutUs.js
@@ -1,25 +1,10 @@
-var releaseVersion='Version : 1.0.10';
-var releaseDate='Release Date : JULY 8 2014';
+var releaseVersion='Version : 1.0.11';
+var releaseDate='Release Date : JULY 9 2014';
 var applicationName='VISion Application';
 function onAboutUs(){
-	document.getElementById('aboutUs').innerHTML="";
-	var br = document.createElement('br');
-	var versionDiv=document.createElement("H4");
-	var realeseDateDiv=document.createElement("H4");
-	var appNameDiv=document.createElement("H2");
-	appNameDiv.innerHTML=applicationName;
-	versionDiv.innerHTML=releaseVersion;
-	realeseDateDiv.innerHTML=releaseDate;
-	document.getElementById('aboutUs').appendChild(appNameDiv);
-	document.getElementById('aboutUs').appendChild(versionDiv);
-	document.getElementById('aboutUs').appendChild(realeseDateDiv);
-	document.getElementById('aboutUs').appendChild(br);
-	var buttonnode= document.createElement('input');
-	buttonnode.setAttribute('type','button');
-	buttonnode.setAttribute('value','Back');
-	buttonnode.className="login_btns";
-	document.getElementById('aboutUs').appendChild(buttonnode);
-	buttonnode.setAttribute('OnClick',"onBackLogin()");
+	document.getElementById('visAppName').innerHTML=applicationName;
+	document.getElementById('visAppVersion').innerHTML=releaseVersion;
+	document.getElementById('visAppRelease').innerHTML=releaseDate;
 	loadPage("aboutUs");
 }
 function onBackLogin(){
