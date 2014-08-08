0a27397df0fa03b7ede3895e75620d5625bb7fbb
Juan Reynoso <jreynoso@velocityelec.com>
2014-08-08 15:30:49 -0500

    Updated build number

--- www/scripts/aboutUs.js
+++ www/scripts/aboutUs.js
@@ -1,5 +1,5 @@
-var releaseVersion='Version : 1.0.14';
-var releaseDate='Release Date : JULY 30 2014';
+var releaseVersion='Version : 1.0.12';
+var releaseDate='Release Date : AUG 8 2014';
 var applicationName='VISion Application';
 function onAboutUs(){
 	document.getElementById('visAppName').innerHTML=applicationName;
