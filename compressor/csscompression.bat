3c8983ad0be75d7e5033e741ecda95bf4ae14cbe
Vijay <Vijay@localhost>
2015-07-01 16:46:22 +0530

    Minify JS files.

--- compressor/csscompression.bat
+++ compressor/csscompression.bat
@@ -3,8 +3,8 @@ cd..
 echo on
 
 CMD /C "echo. 2> compressor\vision_app.min.css"
-CMD /C "java -jar compressor\yuicompressor-2.4.8.jar www\css\themes\vision.min.css > compressor\vision_app.min.css"
+CMD /C "java -jar compressor\yuicompressor-2.4.8.jar www\css\vision.min.css > compressor\vision_app.min.css"
 CMD /C "java -jar compressor\yuicompressor-2.4.8.jar www\css\custom.css >> compressor\vision_app.min.css"
-CMD /C "java -jar compressor\yuicompressor-2.4.8.jar www\js\lib\jmobile\1.4.5\jquery.mobile.structure-1.4.5.min.css >> compressor\vision_app.min.css"
-CMD /C "java -jar compressor\yuicompressor-2.4.8.jar www\css\themes\jquery.mobile.icons.min.css >> compressor\vision_app.min.css"
+CMD /C "java -jar compressor\yuicompressor-2.4.8.jar www\css\jquery.mobile.structure-1.4.5.min.css >> compressor\vision_app.min.css"
+CMD /C "java -jar compressor\yuicompressor-2.4.8.jar www\css\jquery.mobile.icons.min.css >> compressor\vision_app.min.css"
 CMD /C "java -jar compressor\yuicompressor-2.4.8.jar www\css\cropper\cropper.min.css >> compressor\vision_app.min.css"
